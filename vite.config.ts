3ef49ab2b290bfe13bf5632a43242309e89d5534
pythongeek <tsnion@gmail.com>
2025-09-19 01:54:04 +0600

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+import path from 'path';
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    }
+  }
+});
